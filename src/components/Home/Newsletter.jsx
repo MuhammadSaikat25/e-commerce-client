4682c4a134b6ec00394b2a848abb20ed5ed3eabd
Muhammad Saikat <smsaikat000@gmail.com>
2023-12-28 18:06:45 +0600

    added Newsletter section

---
+++ src/components/Home/Newsletter.jsx
@@ -0,0 +1,22 @@
+import banner from "../../assets/Newslatter-banner.png";
+
+const Newsletter = () => {
+  return (
+    <div className="mt-12 p-10" style={{ backgroundImage: `url(${banner})` }}>
+      <div className="flex justify-center items-center gap-12">
+        <div className=" ">
+          <h1 className="text-white font-semibold text-2xl">
+            Sign Up For Our Newsletter
+          </h1>
+          <h1 className="text-white">Subscribe our newsletter and get discount 20% Off</h1>
+        </div>
+        <div className="flex items-center">
+          <input className="border w-full p-1 rounded-l-3xl" type="text" placeholder="Enter your Email"/>
+          <button className="bg-orange-600 text-white p-1 rounded-r-3xl">SUBSCRIBE</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Newsletter;
--- src/page/MainLayout/Home.jsx
+++ src/page/MainLayout/Home.jsx
@@ -2,6 +2,7 @@ import CATEGORY from "../../components/Home/CATEGORY";
 import Discount from "../../components/Home/Discount";
 import HeaderSection from "../../components/Home/HeaderSection";
 import ImageSection from "../../components/Home/ImageSection";
+import Newsletter from "../../components/Home/Newsletter";
 import Parallax from "../../components/Home/Parallax";
 import ShopSafely from "../../components/Home/ShopSafely";
 
@@ -14,6 +15,7 @@ const Home = () => {
       <Discount></Discount>
       <Parallax></Parallax>
       <ShopSafely></ShopSafely>
+      <Newsletter></Newsletter>
     </div>
   );
 };
